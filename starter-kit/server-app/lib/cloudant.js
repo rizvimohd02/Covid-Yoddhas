016cace7b0d35f57273ca06e54f5a11d73a34afb
MohammedRizvi <rizvimohd02@gmail.com>
2020-07-16 12:21:34 +0530

    Add existing file

--- starter-kit/server-app/lib/cloudant.js
+++ starter-kit/server-app/lib/cloudant.js
@@ -17,7 +17,7 @@ var cloudant = new Cloudant({
 
 // Cloudant DB reference
 let db;
-let db_name = "bookings_db";
+let db_name = "community_db";
 
 /**
  * Connects to the Cloudant DB, creating it if does not already exist
@@ -73,21 +73,21 @@ const dbCloudantConnect = () => {
 /**
  * Find all resources that match the specified partial name.
  * 
- * @param {String} place
+ * @param {String} type
  * @param {String} partialName
  * @param {String} userID
  * 
  * @return {Promise} Promise - 
  *  resolve(): all resource objects that contain the partial
- *          name, place or userID provided, or an empty array if nothing
+ *          name, type or userID provided, or an empty array if nothing
  *          could be located that matches. 
  *  reject(): the err object from the underlying data store
  */
-function find(place, partialName, userID) {
+function find(type, partialName, userID) {
     return new Promise((resolve, reject) => {
         let selector = {}
-        if (place) {
-            selector['place'] = place;
+        if (type) {
+            selector['type'] = type;
         }
         if (partialName) {
             let search = `(?i).*${partialName}.*`;
@@ -140,27 +140,29 @@ function deleteById(id, rev) {
 /**
  * Create a resource with the specified attributes
  * 
- * @param {String} place - the place of the item
+ * @param {String} type - the type of the item
  * @param {String} name - the name of the item
- * @param {String} emailid - the emailid of the item
- * @param {String} person - the person available 
+ * @param {String} description - the description of the item
+ * @param {String} quantity - the quantity available 
+ * @param {String} location - the GPS location of the item
  * @param {String} contact - the contact info 
  * @param {String} userID - the ID of the user 
  * 
  * @return {Promise} - promise that will be resolved (or rejected)
  * when the call to the DB completes
  */
-function create(place, name, emailid, person, contact, userID) {
+function create(type, name, description, quantity, location, contact, userID) {
     return new Promise((resolve, reject) => {
         let itemId = uuidv4();
         let whenCreated = Date.now();
         let item = {
             _id: itemId,
             id: itemId,
-            place: place,
+            type: type,
             name: name,
-            emailid: emailid,
-            person: person,
+            description: description,
+            quantity: quantity,
+            location: location,
             contact: contact,
             userID: userID,
             whenCreated: whenCreated
@@ -183,17 +185,18 @@ function create(place, name, emailid, person, contact, userID) {
  * 
  * The following parameters can be null
  * 
- * @param {String} place - the place of the item
+ * @param {String} type - the type of the item
  * @param {String} name - the name of the item
- * @param {String} emailid - the emailid of the item
- * @param {String} person - the person available 
+ * @param {String} description - the description of the item
+ * @param {String} quantity - the quantity available 
+ * @param {String} location - the GPS location of the item
  * @param {String} contact - the contact info 
  * @param {String} userID - the ID of the user 
  * 
  * @return {Promise} - promise that will be resolved (or rejected)
  * when the call to the DB completes
  */
-function update(id, place, name, emailid, person, contact, userID) {
+function update(id, type, name, description, quantity, location, contact, userID) {
     return new Promise((resolve, reject) => {
         db.get(id, (err, document) => {
             if (err) {
@@ -203,10 +206,11 @@ function update(id, place, name, emailid, person, contact, userID) {
                     _id: document._id,
                     _rev: document._rev,            // Specifiying the _rev turns this into an update
                 }
-                if (place) {item["place"] = place} else {item["place"] = document.place};
+                if (type) {item["type"] = type} else {item["type"] = document.type};
                 if (name) {item["name"] = name} else {item["name"] = document.name};
-                if (emailid) {item["emailid"] = emailid} else {item["emailid"] = document.emailid};
-                if (person) {item["person"] = person} else {item["person"] = document.person};
+                if (description) {item["description"] = description} else {item["description"] = document.description};
+                if (quantity) {item["quantity"] = quantity} else {item["quantity"] = document.quantity};
+                if (location) {item["location"] = location} else {item["location"] = document.location};
                 if (contact) {item["contact"] = contact} else {item["contact"] = document.contact};
                 if (userID) {item["userID"] = userID} else {item["userID"] = document.userID};
  

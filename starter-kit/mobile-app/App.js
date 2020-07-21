7ec4981786664ec533375755c71f471a32223afa
MohammedRizvi <rizvimohd02@gmail.com>
2020-07-21 12:07:15 +0530

    Add existing file

--- starter-kit/mobile-app/App.js
+++ starter-kit/mobile-app/App.js
@@ -1,11 +1,11 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
 
-import { Button } from 'react-native';
+import {Button} from 'react-native';
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import LoadingScreen from './src/screens/loading';
 import Home from './src/screens/home';
@@ -14,19 +14,27 @@ import SearchResources from './src/screens/resources-search';
 import AddResource from './src/screens/resource-add';
 import EditResource from './src/screens/resource-edit';
 import MyResources from './src/screens/resources-my';
+
+import EditBooking from './src/screens/booking-edit';
+import MyBookings from './src/screens/bookings-my';
+//import BookAppointment from './src/files/appointments/AddBooking';
+import BookAppointment from './src/screens/booking-add';
+import RegisterBusiness from './src/screens/business-add';
+import ChatMR from './src/screens/chat';
+
 import Map from './src/screens/map';
 
-import { HomeIcon, DonateIcon, SearchIcon } from './src/images/svg-icons';
+import {HomeIcon, DonateIcon, SearchIcon, BookingIcon, BusinessIcon, ChatMRIcon} from './src/images/svg-icons';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
-const ResourcesStackOptions = ({ navigation }) => {
+const ResourcesStackOptions = ({navigation}) => {
   return ({
     headerRight: () => (
       <Button
         onPress={() => navigation.navigate('Chat')}
-        title='Chat '
+        title='Chat'
       />
     )
   });
@@ -34,7 +42,7 @@ const ResourcesStackOptions = ({ navigation }) => {
 
 const DonationsStackOptions = ({ navigation }) => {
   return ({
-    headerRight: () => (
+    headerLeft: () => (
       <Button
         onPress={() => navigation.navigate('Add Donation')}
         title='Add '
@@ -44,12 +52,12 @@ const DonationsStackOptions = ({ navigation }) => {
 };
 
 const tabBarOptions = {
-  // showLabel: false,
+  showLabel: true,
   activeTintColor: '#1062FE',
   inactiveTintColor: '#000',
   style: {
     backgroundColor: '#F1F0EE',
-    paddingTop: 5
+    paddingTop: 0
   }
 };
 
@@ -58,21 +66,40 @@ const TabLayout = () => (
     style={{paddingTop: 50}}
     initialRouteName='Home'
     tabBarOptions={tabBarOptions} >
+    
     <Tab.Screen
       name='Home'
-      component={Home}
+      component={HomeStackLayout}
       options={{
         tabBarIcon: ({color}) => (<HomeIcon fill={color}/>)
       }}
     />
-    <Tab.Screen
-      name='Donate'
-      component={DonateStackLayout}
+
+<Tab.Screen
+      name='Book Appointment'
+      component={BookAppointmentStackLayout}
       options={{
-        tabBarIcon: ({color}) => (<DonateIcon fill={color} />)
+        tabBarIcon: ({color}) => (<BookingIcon fill={color} />)
       }}
     />
-    <Tab.Screen
+
+<Tab.Screen
+      name='Business'
+      component={BusinessStackLayout}
+      options={{
+        tabBarIcon: ({color}) => (<BusinessIcon fill={color} />)
+      }}
+    />
+
+<Tab.Screen
+      name='Ask a Question'
+      component={ChatMRStackLayout}
+      options={{
+        tabBarIcon: ({color}) => (<ChatMRIcon fill={color} />)
+      }}
+    />
+
+<Tab.Screen
       name='Search'
       component={SearchStackLayout}
       options={{
@@ -80,6 +107,13 @@ const TabLayout = () => (
       }}
     />
   </Tab.Navigator>
+);
+
+const HomeStackLayout = () => (
+  <Stack.Navigator>
+   <Stack.Screen name='My Bookings' component={MyBookings} />
+  <Stack.Screen name='Edit Booking' component={EditBooking} />
+  </Stack.Navigator>
 );
 
 const DonateStackLayout = () => (
@@ -98,9 +132,27 @@ const SearchStackLayout = () => (
   </Stack.Navigator>
 );
 
+const BookAppointmentStackLayout = () => (
+  <Stack.Navigator>
+    <Stack.Screen name='New Booking' component={BookAppointment} />
+    <Stack.Screen name='Map' component={Map} />
+  </Stack.Navigator>
+);
+
+const BusinessStackLayout = () => (
+  <Stack.Navigator>
+    <Stack.Screen name='Register Business' component={RegisterBusiness} />
+  </Stack.Navigator>
+);
+
+const ChatMRStackLayout = () => (
+  <Stack.Navigator>
+    <Stack.Screen name='ChatMR' component={ChatMR} />
+  </Stack.Navigator>
+);
+
 const App = () => {
   const [isLoading, setIsLoading] = React.useState(true);
-
 
   React.useEffect(() => {
     setTimeout(() => {
